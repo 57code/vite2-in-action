c56fcf107c8988c11bc25b4b603a66b8e8f84a03
57code <yt0379@sohu.com>
2021-01-17 17:08:04 +0800

    修改test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,6 @@
 <template>
   <h1>{{ msg }}</h1>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank"
-      >Vite Documentation</a
-    >
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
   <button @click="state.count++">count is: {{ state.count }}</button>
   <p>
     Edit
