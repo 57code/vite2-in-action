c0e705a8a4522880df78602430f8d6b305fb25e7
57code <yt0379@sohu.com>
2021-02-18 19:24:11 +0800

    hooks执行顺序

--- plugins/vite-plugin-my-example.js
+++ plugins/vite-plugin-my-example.js
@@ -1,17 +1,57 @@
 export default function myExample () {
+  // 返回的是插件对象
   return {
     name: 'my-example', // 名称用于警告和错误展示
+    // 初始化hooks，只走一次
+    options(opts) {
+      console.log('options', opts);
+    },
+    buildStart() {
+      console.log('buildStart');
+    },
+    config(config) {
+      console.log('config', config);
+      return {}
+    },
+    configResolved(resolvedCofnig) {
+      console.log('configResolved');
+    },
+    configureServer(server) {
+      console.log('configureServer');
+      // server.app.use((req, res, next) => {
+      //   // custom handle request...
+      // })
+    },
+    transformIndexHtml(html) {
+      console.log('transformIndexHtml');
+      return html
+      // return html.replace(
+      //   /<title>(.*?)<\/title>/,
+      //   `<title>Title replaced!</title>`
+      // )
+    },
+    // id确认
     resolveId ( source ) {
       if (source === 'virtual-module') {
+        console.log('resolvedId', source);
         return source; // 返回source表明命中，vite不再询问其他插件处理该id请求
       }
       return null; // 返回null表明是其他id要继续处理
     },
+    // 加载模块代码
     load ( id ) {
       if (id === 'virtual-module') {
+        console.log('load');
         return 'export default "This is virtual!"'; // 返回"virtual-module"模块源码
       }
       return null; // 其他id继续处理
-    }
+    },
+    // 转换
+    transform(code, id) {
+      if (id === 'virtual-module') {
+        console.log('transform');
+      }
+      return code
+    },
   };
 }
